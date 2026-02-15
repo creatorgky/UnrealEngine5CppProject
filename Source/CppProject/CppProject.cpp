1ddf59e4e7c04b76bcd7f6a5ef1170189dd3f848
Hüseyin Gökay Benian <creatorgky@outlook.com>
2026-02-15 13:32:49 +0300

    InitialCommit

---
+++ Source/CppProject/CppProject.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CppProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppProject, "CppProject" );
+
+DEFINE_LOG_CATEGORY(LogCppProject)
---
+++ Source/CppProject/CppProjectGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CppProjectGameMode.h"
+
+ACppProjectGameMode::ACppProjectGameMode()
+{
+	// stub
+}
